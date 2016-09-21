2c3171f0f0b635931723f2ef9f81ab90ff27ce35
天魂无双 <tjpanda88@sina.com>
2016-09-21 09:48:21 +0800

    change file name

--- starry-fw-social/starry-fw-social-wechat/src/main/java/com/github/starry/fw/social/wechat/connect/WechatConnectionFactory.java
+++ starry-fw-social/starry-fw-social-wechat/src/main/java/com/github/starry/fw/social/wechat/connect/WechatConnectionFactory.java
@@ -7,23 +7,22 @@ import com.github.starry.fw.social.wechat.api.Wechat;
 public class WechatConnectionFactory extends OAuth2ConnectionFactory<Wechat> {
 
     /**
-     * Creates a FacebookConnectionFactory for the given application ID and secret.
-     * Using this constructor, no application namespace is set (and therefore Facebook's Open Graph operations cannot be used).
-     * @param appId The application's App ID as assigned by Facebook
-     * @param appSecret The application's App Secret as assigned by Facebook
+     * Creates a WechatConnectionFactory for the given application ID and secret.
+     * @param appId The application's App ID as assigned by Wechat
+     * @param appSecret The application's App Secret as assigned by Wechat
      */
     public WechatConnectionFactory(String appId, String appSecret) {
         this(appId, appSecret, null);
     }
 
     /**
-     * Creates a FacebookConnectionFactory for the given application ID, secret, and namespace.
-     * @param appId The application's App ID as assigned by Facebook
-     * @param appSecret The application's App Secret as assigned by Facebook
-     * @param appNamespace The application's App Namespace as configured with Facebook. Enables use of Open Graph operations.
+     * Creates a WechatConnectionFactory for the given application ID, secret, and namespace.
+     * @param appId The application's App ID as assigned by Wechat
+     * @param appSecret The application's App Secret as assigned by Wechat
+     * @param appNamespace The application's App Namespace as configured with Wechat. Enables use of Open Graph operations.
      */
     public WechatConnectionFactory(String appId, String appSecret, String appNamespace) {
-        super("starry", new WechatServiceProvider(appId, appSecret, appNamespace), new WechatAdapter());
+        super("wechat", new WechatServiceProvider(appId, appSecret, appNamespace), new WechatAdapter());
     }
 
 }
